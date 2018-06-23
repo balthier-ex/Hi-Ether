310f54fd8dccf73a2c49a2169e4e01edcf8a097c
Balthier <34898262+balthier-ex@users.noreply.github.com>
2018-06-23 15:27:13 -0700

    Format code

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 function setupEnvironment() {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
 function daysUntilTarget() {
-    const dayInMilliseconds = 1000 * 60 * 60 * 24;
-    const targetDate = new Date(2018, 11, 7);    
-    const currentDate = new Date();    
-    return Math.ceil((targetDate - currentDate) / (dayInMilliseconds));
+  const dayInMilliseconds = 1000 * 60 * 60 * 24;
+  const targetDate = new Date(2018, 11, 7);
+  const currentDate = new Date();
+  return Math.ceil((targetDate - currentDate) / dayInMilliseconds);
 }
 
 function countdownMessage(daysRemaining) {
-    const product = `${process.env.PRODUCT}`;
-    switch(true) {
-        case (daysRemaining === 0):
-            return `@everyone Today is the release day for ${product}!`;
-        case (daysRemaining === 1):
-            return `@here Get ready for tomorrow! One more day until ${product} release!`;
-        case (daysRemaining > 1):
-            return `${daysRemaining} more days until ${product} release...`;
-        default:
-            return '';
-    }
-}    
+  const product = `${process.env.PRODUCT}`;
+  switch (true) {
+    case daysRemaining === 0:
+      return `@everyone Today is the release day for ${product}!`;
+    case daysRemaining === 1:
+      return `@here Get ready for tomorrow! One more day until ${product} release!`;
+    case daysRemaining > 1:
+      return `${daysRemaining} more days until ${product} release...`;
+    default:
+      return "";
+  }
+}
 
-function webhook() {    
-    const Discord = require('discord.js');    
-    return new Discord.WebhookClient(process.env.ID, process.env.TOKEN);
+function webhook() {
+  const Discord = require("discord.js");
+  return new Discord.WebhookClient(process.env.ID, process.env.TOKEN);
 }
 
 function sendMessageIfNotReleased() {
-    const daysRemaining = daysUntilTarget();
-    if (daysRemaining >= 0) {
-        setupEnvironment();
-        const hook = webhook();
-        const message = countdownMessage(daysRemaining);
-        hook.send(message);
-    }
+  const daysRemaining = daysUntilTarget();
+  if (daysRemaining >= 0) {
+    setupEnvironment();
+    const hook = webhook();
+    const message = countdownMessage(daysRemaining);
+    hook.send(message);
+  }
 }
 
-module.exports = function (context, myTimer) {
-    sendMessageIfNotReleased();
-    context.done();
+module.exports = function(context, myTimer) {
+  sendMessageIfNotReleased();
+  context.done();
 };
